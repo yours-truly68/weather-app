741aa811e154b37f84412ad63dd203f8ad6e4f53
yours-truly68 <mohammedrazim880@gmail.com>
2026-01-09 16:46:34 +0530

    Finish the prjoect

--- server.py
+++ server.py
@@ -5,11 +5,26 @@ from waitress import serve
 app = Flask(__name__)
 
 @app.route('/')
-@app.routep('/index')
+@app.route('/index')
+
 
 
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get("city")
+    weather_data = get_current_weather(city)
+    return render_template(
+        "weather.html",
+        title = weather_data["name"],
+        status = weather_data["weather"][0]["description"].capitalise(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}"
+    )
+
+
 
 if __name__ == "__main__":
     serve(app, host = '0.0.0.0', port = 8000)
